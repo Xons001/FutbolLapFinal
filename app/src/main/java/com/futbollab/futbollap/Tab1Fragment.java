2faea6b0639661969680806b344712c5ebb5c61c
Xons001 <sean.saez.f@gmail.com>
2018-11-22 10:52:04 +0100

    Bug corregido, ahora me toca hacer el carousel en el tab1

--- app/src/main/java/com/futbollab/futbollap/Tab1Fragment.java
+++ app/src/main/java/com/futbollab/futbollap/Tab1Fragment.java
@@ -11,7 +11,7 @@ public class Tab1Fragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+    public View onCreateView (LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.tab1_fragment, container, false);
 
         return view;
--- app/src/main/java/com/futbollab/futbollap/Tab3Fragment.java
+++ app/src/main/java/com/futbollab/futbollap/Tab3Fragment.java
@@ -11,7 +11,7 @@ public class Tab3Fragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+    public View onCreateView (LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.tab3_fragment, container, false);
         return view;
     }
